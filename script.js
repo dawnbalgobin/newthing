41d9547c89d985aa66b0a214e128c4ea18996c0d
Dawn <dawn.balgobin@gmail.com>
2019-05-06 14:29:50 -0400

    adding files

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+"use strict";
+
 const getForm = document.getElementById("userForm");
 getForm.addEventListener("submit", function(e){
     e.preventDefault();
 
-const words = ["sdfas", "sged", "bfdshd", "cat", "ouojnlno", "wetew", "caoasodias", "s", "dc", "tywywunmwah", "nwo", "coagi", "cq", "mnohadoag", "fpeewt", "ui", "baodfnaoegw"];
+const words = ["sdfas", "sged", "bfdshd", "cat", "ouojnlno", "wetew", "caoasodias", "s", "dc", "tywywunmwah", "nwo", "coagi", "cq", "mnohadoag", "fpeewt", "ui", "baodfnaoegw", "bfdsdhs"];
 
 //wrap function in a loop, based on dynamic length user will input
 //style this bish
 let userInput = document.getElementById("userSelect").value;
-console.log(userInput);
+// console.log(userInput);
+for(let i = 0; i <= userInput; i++) {
+    console.log(i);
+    //returns userInput number
 
 const writeWords = () => {
     words.forEach(function() {
         let wordLength = words.length;
         let rando = words[Math.floor(Math.random() * wordLength)];
-        // console.log(rando);
+        console.log(rando);
         document.getElementById("lorem").innerHTML = rando;
          
     });  
 }
 writeWords();
+}
 
 });
